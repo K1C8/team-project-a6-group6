4c79e14f5f09400de839c401a72b5c34be88bd23
ChzuoL <chenzuoliu@gmail.com>
2024-03-20 02:35:48 -0700

    Link mixers and save volume
    Link all the mixers to specific sliders, save the volume when back to main menu

--- Assets/Scripts/OptionsMenu/OptionsMenu.cs
+++ Assets/Scripts/OptionsMenu/OptionsMenu.cs
@@ -3,14 +3,60 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class OptionsMenu : MonoBehaviour
 {
-    public AudioMixer audioMixer;
-    public void SetVolume(float volume)
+    [SerializeField] private AudioMixer audioMixer;
+    [SerializeField] private Slider masterSlider;
+    [SerializeField] private Slider bgmSlider;
+    [SerializeField] private Slider SFXSlider;
+
+    private void Start()
     {
-        //Debug.Log(volume);
+        if (PlayerPrefs.HasKey("masterVol"))
+        {
+            LoadVolume();
+        }
+        else
+        {
+            SetMasterVolume();
+            SetBgmVolume();
+            SetEffectVolume();
+        }
+
+    }
+
+    // This function will control the master volume
+    public void SetMasterVolume()
+    {
+        float volume = masterSlider.value;
         audioMixer.SetFloat("MasterVolume", volume);
+        PlayerPrefs.SetFloat("masterVol", volume);
+    }
+    public void SetBgmVolume()
+    {
+        float volume = bgmSlider.value;
+        audioMixer.SetFloat("bgmVolume", volume);
+        PlayerPrefs.SetFloat("bgmVol", volume);
+    }
+
+    public void SetEffectVolume()
+    {
+        float volume = SFXSlider.value;
+        audioMixer.SetFloat("SoundEffect", volume);
+        PlayerPrefs.SetFloat("sfxVol", volume);
+    }
+
+    private void LoadVolume()
+    {
+        masterSlider.value = PlayerPrefs.GetFloat("masterVol");
+        bgmSlider.value = PlayerPrefs.GetFloat("bgmVol");
+        SFXSlider.value = PlayerPrefs.GetFloat("sfxVol");
+
+        SetMasterVolume();
+        SetBgmVolume();
+        SetEffectVolume();
     }
 
     public void Back() 
