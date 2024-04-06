81a9a1a16db476031514338e4606966de280b219
K1C8 <cjak016@gmail.com>
2024-04-06 11:27:49 -0700

    Implemented basic control of JetPlane player.

--- Assets/Scripts/JetPlane/JetPlayerController.cs
+++ Assets/Scripts/JetPlane/JetPlayerController.cs
@@ -15,11 +15,34 @@ public class JetPlayerController : MonoBehaviour
         
     }
 
+    private Vector3 BoundaryCheckAndMove(float x, float y, float horizontalMovement, float verticalMovement)
+    {
+        if (x < 2.5 && x > -2.5 && y < 4.5 && y > -4.5)
+        {
+            return (Vector3.up * MoveUnit * verticalMovement * Time.deltaTime) + (Vector3.right * MoveUnit * horizontalMovement * Time.deltaTime);
+        } else if (x <= -2.5 || x >= 2.5 || y >= 4.5 || y <= -4.5)
+        {
+            Debug.Log(string.Format("Updating JetPlayer intended as: x {0}, y {1}, horizontalMovement {2}, verticalMovement {3}", x, y, horizontalMovement, verticalMovement));
+            if ((x <= -2.5 || x >= 2.5) && horizontalMovement * x > 0f)
+            {
+                horizontalMovement = 0f;
+            }
+            if ((y <= -4.5 || y >= 4.5) && verticalMovement * y > 0f)
+            {
+                verticalMovement = 0f;
+            }
+            return (Vector3.up * MoveUnit * verticalMovement * Time.deltaTime) + (Vector3.right * MoveUnit * horizontalMovement * Time.deltaTime);
+        }
+        return new Vector3(0f, 0f, 0f);
+    }
+
     // Update is called once per frame
     void Update()
     {
-        float _verticalMovement = Input.GetAxis("Vertical");
-        transform.Translate(Vector3.up * MoveUnit * _verticalMovement * Time.deltaTime);
-        Debug.Log("Updating JetPlayer");
+        float _verticalMovement = Input.GetAxisRaw("Vertical");
+        float _horizontalMovement = Input.GetAxisRaw("Horizontal");
+        float _verticalPosition = transform.position.y;
+        float _horizontalPosition = transform.position.x;
+        transform.Translate(BoundaryCheckAndMove(_horizontalPosition, _verticalPosition, _horizontalMovement, _verticalMovement));
     }
 }
