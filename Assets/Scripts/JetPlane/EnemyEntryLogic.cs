a50bbf22880ee431a325c069ea125c27292ead30
K1C8 <cjak016@gmail.com>
2024-04-08 00:43:36 -0700

    First implementation of enemy unit.

---
+++ Assets/Scripts/JetPlane/EnemyEntryLogic.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class EnemyEntryLogic : MonoBehaviour
+{
+    private float _xStart;
+    private float _xPosition;
+    private float _yPosition;
+    private float _xLeftBound;
+    private float _xRightBound;
+    private float _yLowerBound = 2.5f;
+    private Vector3 _spawnPosition;
+    private int _direction = 1;
+
+    [SerializeField]
+    private float _boardLeftBorder = -2.5f;
+    [SerializeField]
+    private float _boardRightBorder = 2.5f;
+    [SerializeField]
+    private float _speed = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Spawn();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ControlMovement();
+    }
+
+    void Spawn()
+    {
+        _xStart = transform.position.x;
+        _xLeftBound = Math.Max(_boardLeftBorder, _xStart - 1);
+        _xRightBound = Math.Min(_boardRightBorder, _xStart + 1);
+
+        _xPosition = transform.position.x;
+        _spawnPosition = new Vector3(_xPosition, 4, 0);
+        transform.position = _spawnPosition;
+
+
+    }
+
+    void ControlMovement()
+    {
+        IEnumerator DirectionDecision()
+        {
+            yield return new WaitForSecondsRealtime(0.2f);
+            _xPosition = transform.position.x;
+            if (_xPosition < _xLeftBound) 
+            {
+                _direction = 1;
+            } else if (_xPosition > _xRightBound)
+            {
+                _direction = -1;
+            }
+        }
+
+        _xPosition = transform.position.x;
+        _yPosition = transform.position.y;
+
+        StartCoroutine(DirectionDecision());
+
+        if (_yPosition > _yLowerBound)
+        {
+            transform.Translate(Vector3.down * _speed * Time.deltaTime);
+        } else
+        {
+            transform.Translate(Vector3.right * _speed * _direction * Time.deltaTime);
+        }
+        Debug.Log(string.Format("Updating EnemyEntry intended as: x {0}, y {1}, direction {2}", _xPosition, _yPosition, _direction));
+    }
+}
--- Assets/Scripts/JetPlane/JetPlayerController.cs
+++ Assets/Scripts/JetPlane/JetPlayerController.cs
@@ -7,31 +7,51 @@ public class JetPlayerController : MonoBehaviour
 
     [SerializeField] 
     public GameObject JetPlayer;
+    [SerializeField]
+    private GameObject _bulletPrefab;
+    private float _fireInterval = 0.2f;
+    private int _burstCount = 1;
+    private bool _canFire = true;
+    [SerializeField]
     public float MoveUnit;
+
+    private float _boardLowerBorder = 3.5f;
+    private float _boardUpperBorder = -3.5f;
+    private float _boardRightBorder = 2.5f;
+    private float _boardLeftBorder = -2.5f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
-    private Vector3 BoundaryCheckAndMove(float x, float y, float horizontalMovement, float verticalMovement)
+    private Vector3 BoundaryCheckAndMove()
     {
-        if (x < 2.5 && x > -2.5 && y < 4.5 && y > -4.5)
+        float _yMovement = Input.GetAxisRaw("Vertical");
+        float _xMovement = Input.GetAxisRaw("Horizontal");
+        float _yPosition = transform.position.y;
+        float _xPosition = transform.position.x;
+
+        if (_xPosition < _boardRightBorder && _xPosition > _boardLeftBorder && 
+            _yPosition < _boardLowerBorder && _yPosition > _boardUpperBorder)
         {
-            return (Vector3.up * MoveUnit * verticalMovement * Time.deltaTime) + (Vector3.right * MoveUnit * horizontalMovement * Time.deltaTime);
-        } else if (x <= -2.5 || x >= 2.5 || y >= 4.5 || y <= -4.5)
+            return (Vector3.up * MoveUnit * _yMovement * Time.deltaTime) + (Vector3.right * MoveUnit * _xMovement * Time.deltaTime);
+        } else if (_xPosition <= _boardLeftBorder || _xPosition >= _boardRightBorder || 
+            _yPosition >= _boardLowerBorder || _yPosition <= _boardUpperBorder)
         {
-            Debug.Log(string.Format("Updating JetPlayer intended as: x {0}, y {1}, horizontalMovement {2}, verticalMovement {3}", x, y, horizontalMovement, verticalMovement));
-            if ((x <= -2.5 || x >= 2.5) && horizontalMovement * x > 0f)
+            // Debug.Log(string.Format("Updating JetPlayer intended as: x {0}, y {1}, horizontalMovement {2}, verticalMovement {3}", _xPosition, _yPosition, _xMovement, _yMovement));
+            if ((_xPosition <= _boardLeftBorder || _xPosition >= _boardRightBorder) && 
+                _xMovement * _xPosition > 0f)
             {
-                horizontalMovement = 0f;
+                _xMovement = 0f;
             }
-            if ((y <= -4.5 || y >= 4.5) && verticalMovement * y > 0f)
+            if ((_yPosition <= _boardUpperBorder || _yPosition >= _boardLowerBorder) && 
+                _yMovement * _yPosition > 0f)
             {
-                verticalMovement = 0f;
+                _yMovement = 0f;
             }
-            return (Vector3.up * MoveUnit * verticalMovement * Time.deltaTime) + (Vector3.right * MoveUnit * horizontalMovement * Time.deltaTime);
+            return (Vector3.up * MoveUnit * _yMovement * Time.deltaTime) + (Vector3.right * MoveUnit * _xMovement * Time.deltaTime);
         }
         return new Vector3(0f, 0f, 0f);
     }
@@ -39,10 +59,24 @@ public class JetPlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float _verticalMovement = Input.GetAxisRaw("Vertical");
-        float _horizontalMovement = Input.GetAxisRaw("Horizontal");
-        float _verticalPosition = transform.position.y;
-        float _horizontalPosition = transform.position.x;
-        transform.Translate(BoundaryCheckAndMove(_horizontalPosition, _verticalPosition, _horizontalMovement, _verticalMovement));
+
+        IEnumerator BulletTimer()
+        {
+            yield return new WaitForSecondsRealtime(_fireInterval);
+            _canFire = true;
+        }
+
+        if (_canFire)
+        {
+            for (int i = 0; i < _burstCount; i++)
+            {
+                Instantiate(_bulletPrefab, transform.position + new Vector3(0f, 0.25f, 0f), Quaternion.identity);
+            }
+            _canFire = false;
+            StartCoroutine(BulletTimer());
+        }
+
+        transform.Translate(BoundaryCheckAndMove());
+
     }
 }
