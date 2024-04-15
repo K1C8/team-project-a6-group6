318c5d170b1cf4576dab878538046c95d257c5ef
Kaiyue Lin <lin.kaiyu@northeastern.edu>
2024-04-15 16:16:31 -0700

    Revert "Merge pull request #11 from CS5520FeinbergSpring2024/Tetris-new-UI-template"
    This reverts commit 0e35592832378b0b46efb30f61607e3de6f283be, reversing
    changes made to 779b1cb7883de503e09426357b4750290007acf8.

--- Assets/Resources/Tetris/Scripts/Piece.cs
+++ Assets/Resources/Tetris/Scripts/Piece.cs
@@ -11,27 +11,13 @@ public class Piece : MonoBehaviour
     public Vector3Int[] cells { get; private set; }
     public Vector3Int position { get; private set; }
     public int rotationIndex { get; private set; } // (0,1,2,3) storing 4 rotations
-    [HideInInspector]
-    public bool isGameStarted;
 
     public float stepDelay;
-    public float lockDelay;
+    public float lockDelay = .5f;
 
     private float stepTime;
     private float lockTime; // time for locking after the tetris touches the ground?
     private bool isLocked;
-
-    public void OnGameStart()
-    {
-        this.isGameStarted = true;
-    }
-
-    private void Start()
-    {
-        this.isLocked = false;
-        this.isGameStarted = false;
-        //Debug.Log("Piece: started");
-    }
 
     public void Initialize(Board board, Vector3Int position, TetrominoData data, float stepDelay)
     {
@@ -39,10 +25,10 @@ public class Piece : MonoBehaviour
         this.position = position;
         this.data = data;
         this.rotationIndex = 0;
-        this.stepDelay = stepDelay;
         this.stepTime = Time.time + this.stepDelay; // trigger a stepping each stepDelay amount of time
         this.lockTime = 0f;
-        this.isLocked = (this.stepDelay > 10) ?  true : false;
+        this.isLocked = false;
+        this.stepDelay = stepDelay;
 
         if (this.cells == null)
         {
@@ -53,74 +39,42 @@ public class Piece : MonoBehaviour
         { 
             this.cells[i] = ((Vector3Int)data.cells[i]);
         }
-
-        //Debug.Log("Tetrimono get initialized! stepTime: " + stepTime.ToString());
     }
 
     public void Update()
     {
-        if (this.isLocked || !this.isGameStarted) { return; }
+        if (this.isLocked) { return; }
 
-        //if (Time.time % 2 >= 1) Debug.Log("Time: " + Time.time);
-
+        this.board.ClearTile(this);
         this.lockTime += Time.deltaTime;
 
         if (Input.GetKeyDown(KeyCode.A))
         {
-            this.board.ClearTile(this);
             Move(Vector2Int.left);
-            this.board.Set(this);
         }
         else if (Input.GetKeyDown(KeyCode.D))
         {
-            this.board.ClearTile(this);
             Move(Vector2Int.right);
-            this.board.Set(this);
         }
         else if (Input.GetKeyDown(KeyCode.S))
         {
-            this.board.ClearTile(this);
             Move(Vector2Int.down);
-            this.board.Set(this);
         }
         else if (Input.GetKeyDown(KeyCode.Space))
         {
-            this.board.ClearTile(this);
             HardDrop();
-            this.board.Set(this);
         }
         else if (Input.GetKeyDown(KeyCode.W))
         {
-            this.board.ClearTile(this);
             Rotate();
-            this.board.Set(this);
         }
+        this.board.Set(this);
         
         if (Time.time >= this.stepTime)
         {
             Step();
-            Debug.Log("Piece | Step!");
         }
     
-    }
-    public void VirtualMove(int direction)
-    {
-        if (this.isLocked || !this.isGameStarted) { return; }
-
-        this.board.ClearTile(this);
-
-        if (direction == 1)
-            Move(Vector2Int.left);
-        else if (direction == 2)
-            Move(Vector2Int.right);
-        else if (direction == 3)
-            Move(Vector2Int.down);
-        else if (direction == 4)
-            Rotate();
-        else if (direction == 5)
-            HardDrop();
-
-        this.board.Set(this);
     }
 
     private void Step()
@@ -148,7 +102,7 @@ public class Piece : MonoBehaviour
         this.board.Set(this);
         this.isLocked = true;
 
-        if (!this.board.IsGameOver)
+        if (!this.board.isGameOver)
         {
             this.lockTime = 0f;
             this.board.ClearRows();
@@ -156,11 +110,17 @@ public class Piece : MonoBehaviour
         }
     }
 
+    private void HardDrop()
+    {
+        while (Move(Vector2Int.down))
+        {
+            continue;
+        }
+        Lock();
+    }
 
     private bool Move(Vector2Int translation)
     {
-        if (!this.isGameStarted) { return false; }
-
         Vector3Int newPosition = this.position;
         newPosition.x += translation.x;
         newPosition.y += translation.y;
@@ -174,15 +134,6 @@ public class Piece : MonoBehaviour
         }
 
         return valid;
-    }
-
-    private void HardDrop()
-    {
-        while (Move(Vector2Int.down))
-        {
-            continue;
-        }
-        Lock();
     }
 
     private void Rotate()
