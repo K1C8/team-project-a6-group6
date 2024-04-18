8d9b421090e24d4d7e027be1a026da0aa142e7d4
K1C8 <cjak016@gmail.com>
2024-04-18 01:25:46 -0700

    Changing InGameJet scene name to Jet

--- Assets/Scripts/MainMenuUI/MainMenuUIController.cs
+++ Assets/Scripts/MainMenuUI/MainMenuUIController.cs
@@ -81,7 +81,7 @@ public class MainMenuUIController : MonoBehaviour
     }
     public void PlayJet()
     {
-        SceneManager.LoadScene("InGameJet");
+        SceneManager.LoadScene("Jet");
         AudioManager.Instance.StopMusic();
     }
 }
